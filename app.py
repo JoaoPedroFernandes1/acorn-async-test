b7a0eaca698ef9c819680f0251d37ffe82ec5d91
João Pedro Fernandes <j.fernandes@acornad.com.br>
2025-03-11 12:07:33 -0300

    Testes de rotas

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello World!"
+
+@app.route('/rota2')
+def home2():
+    return "<H1> Testando segunda rota!!!"
+
+@app.route('/test_parameter/<string:nome>/<int:idade>')
+def pessoa(nome, idade):
+    return jsonify({'Nome': nome, 'Idade': idade})
 
 if __name__ == "__main__":
     app.run(debug=True)
