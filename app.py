5c6163387f970d11cccd156c946da76facc76aee
João Pedro Fernandes <j.fernandes@acornad.com.br>
2025-03-14 10:32:56 -0300

    Alterações no Polling

--- app.py
+++ app.py
@@ -11,29 +11,33 @@ tasks = {}
 
 #! FUNÇÃO: Força a aplicação a esperar 70 segundos antes de responder para verificar o erro de Timeout
 def long_task(task_id):
+    print("Tarefa iniciada!")  # Confirmação da inicialização
+    #sys.stdout.flush()  # Força a exibição do print nos logs
     time.sleep(70)  # Simula uma tarefa longa
-    tasks[task_id] = "Concluído"
+    tasks[task_id] = "Tarefa Concluida"
+    print("Tarefa concluída!")  # É para aparecer após 70s
+    #sys.stdout.flush()
 
 
-#? ROTA: PADRÃO
+#? ROTA 1: PADRÃO
 @app.route('/')
 def home():
     return "Hello World!"
 
 
-#? ROTA: TESTE DE MUDANÇA DE ROTA
+#? ROTA 2: TESTE DE MUDANÇA DE ROTA
 @app.route('/rota2')
 def home2():
     return "<H1> Testando segunda rota!!!"
 
 
-#? ROTA: TESTE DE PARÂMETROS NA ROTA
+#? ROTA 3: TESTE DE PARÂMETROS NA ROTA
 @app.route('/test_parameter/<string:nome>/<int:idade>')
 def pessoa(nome, idade):
     return jsonify({'Nome': nome, 'Idade': idade})
 
 
-#? ROTA: INDUZINDO AO ERRO H12 DE TIMEOUT E POR ELA, SOLUÇÕES
+#? ROTA 4: INDUZINDO AO ERRO H12 DE TIMEOUT E POR ELA, SOLUÇÕES
 @app.route('/timeout')
 def timeout():
     thread = threading.Thread(target=long_task)
@@ -41,10 +45,11 @@ def timeout():
     return jsonify({"message": "Tarefa iniciada, verifique os logs para status do time.sleep"}), 202
 
 
+#! ROTA 5: INICIA UMA TAREFA
 @app.route('/start')
 def start_task():
     task_id = str(len(tasks) + 1)  # Gera um ID simples
-    tasks[task_id] = "Em andamento"
+    tasks[task_id] = "Tarefa em andamento"
     
     thread = threading.Thread(target=long_task, args=(task_id,))
     thread.start()
@@ -52,9 +57,10 @@ def start_task():
     return jsonify({"message": "Tarefa iniciada", "task_id": task_id}), 202
 
 
+#! ROTA 6: VERIFICA O STATUS DA TAREFA e É COMPLEMENTAR A ROTA 5
 @app.route('/status/<task_id>')
 def get_status(task_id):
-    status = tasks.get(task_id, "Não encontrada")
+    status = tasks.get(task_id, "Tarefa nao encontrada")
     return jsonify({"task_id": task_id, "status": status})
 
 # Main
